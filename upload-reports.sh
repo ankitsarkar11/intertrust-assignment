d1d1e57c49b49c250d8e2155e3ac1c2231b4deb2
Ankit Sarkar <sankit@intertrust.com>
2023-10-11 09:14:45 +0530

    add allure executable and script

--- upload-reports.sh
+++ upload-reports.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-REPO_HOME=/data/repo
-$REPO_HOME/allure-2.21.0/bin/allure serve --host localhost --port 34560
+cd /data/repo
+./allure-2.21.0/bin/allure serve --host localhost --port 34560
